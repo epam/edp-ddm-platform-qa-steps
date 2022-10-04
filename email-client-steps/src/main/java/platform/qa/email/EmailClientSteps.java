39876cc3bf7e57ba032213ab0258dc51b47733a3
Yana Drozd <yana_drozd@epam.com>
2022-10-04 11:05:01 +0300

    [EDP-DDM-12731] Updated email client steps
    Change-Id: Iaaeaf16dcc562978308a6341c0dd01a796132542

--- email-client-steps/src/main/java/platform/qa/email/EmailClientSteps.java
+++ email-client-steps/src/main/java/platform/qa/email/EmailClientSteps.java
@@ -1,8 +1,23 @@
+/*
+ * Copyright 2022 EPAM Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package platform.qa.email;
 
 import io.restassured.common.mapper.TypeRef;
-import platform.qa.email.models.mailboxListResponse.MailUserModel;
-import platform.qa.email.models.messageResponseModel.MailUserByIdModel;
+import platform.qa.email.entities.response.UserMail;
+import platform.qa.email.entities.response.UserMailMessage;
 import platform.qa.email.service.EmailService;
 import platform.qa.entities.User;
 
@@ -11,19 +26,19 @@ import java.util.List;
 public class EmailClientSteps {
     private final EmailService emailService = new EmailService();
 
-    public List<MailUserModel> getUserMails(User user) {
-        return emailService.getMailboxListByUser(user.getLogin())
+    public List<UserMail> getAllUserMails(User user) {
+        return emailService.getAllUserMails(user.getLogin())
                 .statusCode(200)
                 .extract()
                 .body()
                 .as(new TypeRef<>() {});
     }
 
-    public MailUserByIdModel getUserMailData(User user, String mailId) {
-        return emailService.getMailInfoByUserNameAndMailId(user.getLogin(), mailId)
+    public UserMailMessage getUserMailContent(User user, String mailId) {
+        return emailService.getUserMailById(user.getLogin(), mailId)
                 .statusCode(200)
                 .extract()
                 .body()
-                .as(MailUserByIdModel.class);
+                .as(UserMailMessage.class);
     }
 }
